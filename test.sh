e8e96303b8d0036841e97f552f084e99280f0f07
deanboole <xspiritualx@gmail.com>
2015-01-08 18:44:36 +0800

    Add shunit2 submodule

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash - 
+
+test_CHECK_SHELL_ENV(){
+	ls_shell_output=$(ls -l `which sh`|grep -o dash)
+	assertNotSame 'change dash to bash' "dash" "$ls_shell_output"
+}
+
+. shunit2/src/shunit2
